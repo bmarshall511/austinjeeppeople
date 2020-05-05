074b3e2b79e41711c1441d0896c628e948c0c9e5
Ben Marshall <bmarshall511@gmail.com>
2020-05-05 14:15:25 -0500

    Updates

--- wp-content/plugins/ajp-recommendations/inc/post-types.php
+++ wp-content/plugins/ajp-recommendations/inc/post-types.php
@@ -54,7 +54,7 @@ if ( ! function_exists('ajprecommendations_post_types') ) {
       'description'           => __( 'AJP member recommendations for local businesses.', 'ajprecommendations' ),
       'labels'                => $labels,
       'show_in_rest'          => true,
-      'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'excerpt' ),
+      'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'excerpt', 'comments' ),
       'taxonomies'            => array( 'category', 'post_tag' ),
       'hierarchical'          => false,
       'public'                => true,
