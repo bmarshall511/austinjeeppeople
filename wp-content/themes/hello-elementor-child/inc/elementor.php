507802e5eb0cbc350337e43124e2941fcd778608
Ben Marshall <bmarshall511@gmail.com>
2020-05-06 12:02:59 -0500

    Updates

--- wp-content/themes/hello-elementor-child/inc/elementor.php
+++ wp-content/themes/hello-elementor-child/inc/elementor.php
@@ -6,3 +6,8 @@
  * @subpackage HelloElementorChild
  * @since 1.0.0
  */
+
+// topics_filter Elementor query ID - Displays forum topics.
+add_action( 'elementor/query/topics_filter', function( $query ) {
+  $query->set( 'post_type', 'topic' );
+});
