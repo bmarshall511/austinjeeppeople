4ba52d53be42353a0db7532a85b8303770579ecb
Ben Marshall <bmarshall511@gmail.com>
2020-05-06 12:46:10 -0500

    Updates

--- wp-content/themes/hello-elementor-child/inc/shortcodes.php
+++ wp-content/themes/hello-elementor-child/inc/shortcodes.php
@@ -6,3 +6,9 @@
  * @subpackage HelloElementorChild
  * @since 1.0.0
  */
+
+ // [the_content]
+function hello_elementor_child_shortcode_the_content( $atts ) {
+	the_content();
+}
+add_shortcode( 'the_content', 'hello_elementor_child_shortcode_the_content' );
